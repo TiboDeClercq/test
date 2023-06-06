c536784a4e1c3c2be0cbcd23e869dbba191db057
Tibo De Clercq <tdc@riskconcile.com>
2023-06-06 16:56:57 +0200

    x

--- python.py
+++ python.py
@@ -1,25 +1,79 @@
+
+import inspect
+import re
 import boto3
 
-def lambda_handler(event, context):
-    # Initialize the AWS clients for SQS, S3 and Textract
-    sqs_client = boto3.client('sqs')
-    s3_client = boto3.client('s3')
-    textract_client = boto3.client('textract')
+#TODO error handeling
 
-    # Get the SQS message containing the S3 object key
-    sqs_message = sqs_client.receive_message(QueueUrl='YOUR_SQS_QUEUE_URL')['Messages'][0]
-    s3_object_key = sqs_message['Body']
+class TextractUploader:
+    def __init__(self, role_arn, session_name):
+        self.role_arn = role_arn
+        self.session_name = session_name
+        self.s3_client = self._create_s3_client()
+        
+    def _create_s3_client(self):
+        sts_client = boto3.client('sts')
+        #Assume the role
+        response = sts_client.assume_role(
+            RoleArn=self.role_arn,
+            RoleSessionName=self.session_name
+        )
 
-    # Delete the SQS message
-    sqs_client.delete_message(QueueUrl='YOUR_SQS_QUEUE_URL', ReceiptHandle=sqs_message['ReceiptHandle'])
+        credentials = response['Credentials']
+        access_key = credentials['AccessKeyId']
+        secret_key = credentials['SecretAccessKey']
+        session_token = credentials['SessionToken']
 
-    # Fetch the S3 object
-    s3_bucket_name = 'YOUR_S3_BUCKET_NAME'
-    s3_object = s3_client.get_object(Bucket=s3_bucket_name, Key=s3_object_key)
+        return boto3.client(
+            's3',
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            aws_session_token=session_token
+        )
+    
+    
+    def upload_file(self, file_name, file_path, bucket_name, subsystem, project, textract_process):
+        file_name = file_name
+        file_path = file_path
+        bucket_name = bucket_name
+        subsystem = subsystem  
+        project = project    
+        textract_process = textract_process
+        
+        #check if S3 bucket exists
+        if self.s3_client.head_bucket(Bucket=bucket_name):
+        
 
-    # Send the object content to Textract for analysis
-    response = textract_client.analyze_document(Document={'Bytes': s3_object['Body'].read()})
+        if _check_textract_service(textract_process):
+            destination_path = f"s3://{bucket_name}/incoming/{subsystem}/{project}/{textract_process}/{file_name}"
+            self.s3_client.upload_file(file_path, bucket_name, destination_path)
 
-    # Do something with the Textract response, such as sending it to an SNS topic or saving it to a database
-    print(response)
+        
+        # check if folder exists within a bucket, if not create that folder
+        if not self.s3_client.list_objects(Bucket=bucket_name, Prefix=f"incoming/{subsystem}/"):
+            self.s3_client.put_object(Bucket=bucket_name, Key=f"incoming/{subsystem}/")
+            if not self.s3_client.list_objects(Bucket=bucket_name, Prefix=f"incoming/{subsystem}/{project}/"):
+                self.s3_client.put_object(Bucket=bucket_name, Key=f"incoming/{subsystem}/{project}/")
+                if not self.s3_client.list_objects(Bucket=bucket_name, Prefix=f"incoming/{subsystem}/{project}/{textract_process}/"):
+                    self.s3_client.put_object(Bucket=bucket_name, Key=f"incoming/{subsystem}/{project}/{textract_process}/")
+                    self.s3_client.upload_file(file_path, bucket_name, destination_path)
 
+
+        
+        
+        # self.s3_client.upload_file(file_path, bucket_name, s3_key)
+
+
+    
+
+#print all functions from the textract client
+def _check_textract_service(args):
+    for name, data in inspect.getmembers(boto3.client('textract')):
+        if name.startswith('_'):
+            continue
+        if inspect.ismethod(data):
+            return True
+        else:
+            return False
+
+#check if folder exists within a bucket

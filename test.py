3ac62b574e13153f1ac1c50e709502e5b07e1f3f
Tibo De Clercq <tdc@riskconcile.com>
2023-06-21 13:32:44 +0200

    x

--- test.py
+++ test.py
@@ -16,3 +16,14 @@ from botocore.exceptions import NoCredentialsError
 # one to another the specific folder in another bucket
 # and tag them with the new bucket name
 def copy_files(source_bucket, destination_bucket, source_prefix, destination_prefix):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(source_bucket)
+    for obj in bucket.objects.filter(Prefix=source_prefix):
+        copy_source = {
+            'Bucket': source_bucket,
+            'Key': obj.key
+        }
+        destination_key = obj.key.replace(source_prefix, destination_prefix, 1)
+        s3.meta.client.copy(copy_source, destination_bucket, destination_key)
+        s3.Object(destination_bucket, destination_key).put(Tagging='bucket=' + destination_bucket)
+        s3.Object(source_bucket, obj.key).delete()

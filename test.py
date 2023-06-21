e0f637001d7be862504a021603736edbd6e22e57
Tibo De Clercq <tdc@riskconcile.com>
2023-06-21 13:31:57 +0200

    x

--- test.py
+++ test.py
@@ -13,22 +13,6 @@ from botocore.exceptions import NoCredentialsError
 
 
 #iterate over a S3 bucket and move the files from 
-# one to another bucket
+# one to another the specific folder in another bucket
 # and tag them with the new bucket name
 def copy_files(source_bucket, destination_bucket, source_prefix, destination_prefix):
-
-    #create s3 resource
-    s3 = boto3.resource('s3')
-    #get the source bucket
-    source_bucket = s3.Bucket(source_bucket)
-    #get the destination bucket
-    destination_bucket = s3.Bucket(destination_bucket)
-    
-    #iterate over the source bucket
-    for obj in source_bucket.objects.filter(Prefix=source_prefix):
-        #copy the object
-        destination_bucket.copy({'Bucket': source_bucket.name, 'Key': obj.key}, obj.key)
-        #tag the object
-        destination_bucket.Object(obj.key).Tagging().put(Tagging={'TagSet': [{'Key': 'bucket', 'Value': destination_bucket.name}]})
-
-    print("Done")
